77672aa3b2e9d77cad377eb569d72bf511fc0030
Kris Austin <kris.austin@gmail.com>
2012-11-16 18:11:14 +0000

    Updated Context Processor to return static URL if static files storage is not cumulus.  This allows the use of {{ CDN_URL }} in templates when using the default static files storage.  This is important for development environments that are not likely to be storing static files in the CDN.

--- cumulus/context_processors.py
+++ cumulus/context_processors.py
@@ -7,9 +7,13 @@ def cdn_url(request):
     A context processor to expose the full cdn url in templates.
 
     """
-    cloudfiles_storage = CloudFilesStorage()
     static_url = settings.STATIC_URL
-    container_url = cloudfiles_storage._get_container_url()
-    cdn_url = container_url + static_url
+
+    if settings.STATICFILES_STORAGE == 'cumulus.storage.CloudFilesStaticStorage':
+        cloudfiles_storage = CloudFilesStorage()
+        container_url = cloudfiles_storage._get_container_url()
+        cdn_url = container_url + static_url
+    else:
+        cdn_url = static_url
 
     return {'CDN_URL': cdn_url}
