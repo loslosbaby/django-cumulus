835d02393d2c26de195e420ae39df87589058178
Kris Austin <kris.austin@gmail.com>
2013-06-14 21:08:53 +0000

    Hard coded cdn url.

--- cumulus/context_processors.py
+++ cumulus/context_processors.py
@@ -9,10 +9,11 @@ def cdn_url(request):
     """
     static_url = settings.STATIC_URL
 
-    if settings.STATICFILES_STORAGE == 'cumulus.storage.CloudFilesStaticStorage':
-        cloudfiles_storage = CloudFilesStaticStorage()
-        container_url = cloudfiles_storage._get_container_url()
-        cdn_url = container_url + static_url
+    if settings.STATICFILES_STORAGE == 'cumulus.storage.CloudFilesStaticStorage' and not settings.DEBUG:
+        #cloudfiles_storage = CloudFilesStaticStorage()
+        #container_url = cloudfiles_storage._get_container_url()
+        #cdn_url = container_url + static_url
+        cdn_url = 'https://3fe98249bc30b3d0113c-8e627ca5b66feb89c2661eae1a2cc475.ssl.cf2.rackcdn.com/static/'
     else:
         cdn_url = static_url
 
