0faa052ccb6c9ccfd26390ee40c1ad688774e259
Kris Austin <kris.austin@gmail.com>
2013-08-09 17:20:16 +0000

    Context processor now just uses settings.CDN_URL

--- cumulus/context_processors.py
+++ cumulus/context_processors.py
@@ -7,14 +7,4 @@ def cdn_url(request):
     A context processor to expose the full cdn url in templates.
 
     """
-    static_url = settings.STATIC_URL
-
-    if settings.STATICFILES_STORAGE == 'cumulus.storage.CloudFilesStaticStorage' and not settings.DEBUG:
-        #cloudfiles_storage = CloudFilesStaticStorage()
-        #container_url = cloudfiles_storage._get_container_url()
-        #cdn_url = container_url + static_url
-        cdn_url = 'https://3fe98249bc30b3d0113c-8e627ca5b66feb89c2661eae1a2cc475.ssl.cf2.rackcdn.com/static/'
-    else:
-        cdn_url = static_url
-
-    return {'CDN_URL': cdn_url}
+    return {'CDN_URL': settings.CDN_URL}
